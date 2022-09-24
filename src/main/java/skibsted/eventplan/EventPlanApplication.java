b510774fe70c12551b0620e88ec14269dd3f5d51
SkibstedN <niko933n@stud.kea.dk>
2022-09-24 14:09:20 +0200

    review and venue classes

---
+++ src/main/java/skibsted/eventplan/EventPlanApplication.java
@@ -0,0 +1,13 @@
+package skibsted.eventplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventPlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skibsted/eventplan/EventPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package skibsted.eventplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
